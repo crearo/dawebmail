75cf45221aced3dfe081736345aa837627f797d8
CreaRo <bhardwaj.rish@gmail.com>
2016-06-14 00:07:58 +0530

    Refactored ViewEmail

--- app/src/main/java/com/sigmobile/dawebmail/ViewEmail.java
+++ app/src/main/java/com/sigmobile/dawebmail/ViewEmail.java
@@ -7,7 +7,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
@@ -22,7 +21,6 @@ import com.sigmobile.dawebmail.database.EmailMessage;
 import com.sigmobile.dawebmail.database.User;
 import com.sigmobile.dawebmail.database.UserSettings;
 import com.sigmobile.dawebmail.utils.BasePath;
-import com.sigmobile.dawebmail.utils.ConnectionManager;
 import com.sigmobile.dawebmail.utils.Constants;
 import com.sigmobile.dawebmail.utils.DateUtils;
 import com.sigmobile.dawebmail.utils.FileUtils;
@@ -32,74 +30,49 @@ import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import me.drakeet.materialdialog.MaterialDialog;
 
 public class ViewEmail extends AppCompatActivity implements ViewMailListener {
 
     private static final String TAG = "ViewEmail";
 
-    EmailMessage currentEmail;
-    String EMAIL_TYPE;
-
-    ProgressDialog progdialog;
+    private EmailMessage currentEmail;
+    private String EMAIL_TYPE;
+    private ProgressDialog progressDialog;
+    private User currentUser;
 
     @Bind(R.id.view_tool_bar)
     Toolbar toolbar;
-
     @Bind(R.id.viewmail_sender)
-    TextView tvsender;
-
+    TextView senderName;
     @Bind(R.id.viewmail_subject)
-    TextView tvsubject;
-
+    TextView subject;
     @Bind(R.id.viewmail_senderbottom)
-    TextView tvsenderbottom;
-
+    TextView senderNameBottom;
     @Bind(R.id.viewmail_datebottom)
-    TextView tvdatebottom;
-
+    TextView dateBottom;
     @Bind(R.id.viewmail_webview)
-    WebView webView_viewContent;
-
+    WebView contentContainer;
     @Bind(R.id.viewmail_attach_ll)
-    LinearLayout ll_attachments;
-
-    User currentUser;
+    LinearLayout attachmentsContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_email);
-
         ButterKnife.bind(this);
 
         currentUser = UserSettings.getCurrentUser(this);
+        progressDialog = new ProgressDialog(ViewEmail.this);
 
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendRefreshBroadcast();
-                finish();
-            }
-        });
+        setupToolbar();
+        setupInternalBroadcastReceivers();
+        showEmailContent();
 
-        Bundle bundle = this.getIntent().getExtras();
-        if (bundle != null) {
-            currentEmail = (EmailMessage) bundle.getSerializable(Constants.CURRENT_EMAIL_SERIALIZABLE);
-            EMAIL_TYPE = bundle.getString(Constants.CURRENT_EMAIL_TYPE);
-            long EMAIL_ID = bundle.getLong(Constants.CURRENT_EMAIL_ID);
-            if (EMAIL_ID != -1)
-                currentEmail.setId(EMAIL_ID);
-        }
+    }
 
-        progdialog = new ProgressDialog(ViewEmail.this);
-
-        getSupportActionBar().setTitle("@" + currentEmail.fromName);
-
+    private void showEmailContent() {
         if (currentEmail.content.equals("") || currentEmail.content == null) {
-            setEmailContent("<html><head></head><body>Connect to the Internet to download content</body></html>");
+            setEmailContent(getString(R.string.error_connect_to_view_email));
             /*
             *since we dont have to sendMsg in to view the webmail anymore
             * (since basic auth in restapi provides us to be logged in always)
@@ -111,6 +84,31 @@ public class ViewEmail extends AppCompatActivity implements ViewMailListener {
         }
     }
 
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRefreshBroadcast();
+                finish();
+            }
+        });
+        getSupportActionBar().setTitle("@" + currentEmail.fromName);
+    }
+
+    private void setupInternalBroadcastReceivers() {
+        Bundle bundle = this.getIntent().getExtras();
+        if (bundle != null) {
+            currentEmail = (EmailMessage) bundle.getSerializable(Constants.CURRENT_EMAIL_SERIALIZABLE);
+            EMAIL_TYPE = bundle.getString(Constants.CURRENT_EMAIL_TYPE);
+            long EMAIL_ID = bundle.getLong(Constants.CURRENT_EMAIL_ID);
+            if (EMAIL_ID != -1)
+                currentEmail.setId(EMAIL_ID);
+        }
+    }
+
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -120,8 +118,8 @@ public class ViewEmail extends AppCompatActivity implements ViewMailListener {
 
     @Override
     public void onPreView() {
-        progdialog = ProgressDialog.show(ViewEmail.this, "", "Fetching Content.", true);
-        progdialog.setCancelable(false);
+        progressDialog = ProgressDialog.show(ViewEmail.this, "", getString(R.string.dialog_fetching_content), true);
+        progressDialog.setCancelable(false);
     }
 
     @Override
@@ -131,57 +129,30 @@ public class ViewEmail extends AppCompatActivity implements ViewMailListener {
             if (currentEmail.readUnread.equals(Constants.WEBMAIL_UNREAD))
                 markWebmailAsRead();
 
-            Log.d(TAG, "Supposed to save this email " + currentEmail.getId());
-
             setEmailContent(emailMessage.content);
             emailMessage.readUnread = Constants.WEBMAIL_READ;
             if (EMAIL_TYPE.equals(Constants.INBOX)) {
                 emailMessage.save();
-                Log.d(TAG, "Saving this email " + emailMessage.getId());
             }
             currentEmail = emailMessage;
         } else {
-            setEmailContent("<html><head></head><body>Connect to the Internet to download content</body></html>");
+            setEmailContent(getString(R.string.error_connect_to_view_email));
         }
-        progdialog.dismiss();
+        progressDialog.dismiss();
     }
 
-
-    public void createDownloadDialog(final int whichAttatchment) {
-        final MaterialDialog materialDialog = new MaterialDialog(ViewEmail.this);
-        materialDialog.setTitle("Download the attachment?");
-        if (ConnectionManager.isConnectedByMobileData(ViewEmail.this))
-            materialDialog.setMessage("You are conected over mobile network.");
-        if (ConnectionManager.isConnectedByWifi(ViewEmail.this))
-            materialDialog.setMessage("You are conected over wifi network.");
-
-        materialDialog.setNegativeButton("No", new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                materialDialog.dismiss();
-            }
-        });
-
-        materialDialog.setPositiveButton("Download", new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                materialDialog.dismiss();
-            }
-        });
-        materialDialog.show();
-    }
 
     public void setEmailContent(String html) {
         String mime = "text/html";
         String encoding = "utf-8";
 
-        webView_viewContent.getSettings().setJavaScriptEnabled(true);
-        webView_viewContent.loadDataWithBaseURL(null, html, mime, encoding, null);
+        contentContainer.getSettings().setJavaScriptEnabled(true);
+        contentContainer.loadDataWithBaseURL(null, html, mime, encoding, null);
 
-        tvsender.setText(currentEmail.fromAddress);
-        tvsubject.setText(currentEmail.subject);
-        tvsenderbottom.setText(currentEmail.fromName);
-        tvdatebottom.setText(DateUtils.getDate(getApplicationContext(), Long.parseLong(currentEmail.dateInMillis)));
+        senderName.setText(currentEmail.fromAddress);
+        subject.setText(currentEmail.subject);
+        senderNameBottom.setText(currentEmail.fromName);
+        dateBottom.setText(DateUtils.getDate(getApplicationContext(), Long.parseLong(currentEmail.dateInMillis)));
 
         final ArrayList<String> attachmentsList = BasePath.getAttachmentsPaths(getApplicationContext(), currentEmail.contentID);
         for (int i = 0; i < attachmentsList.size(); i++) {
@@ -198,7 +169,7 @@ public class ViewEmail extends AppCompatActivity implements ViewMailListener {
                     FileUtils.openDoc(getApplicationContext(), attachmentsList.get(finalI));
                 }
             });
-            ll_attachments.addView(textView);
+            attachmentsContainer.addView(textView);
         }
     }
 
@@ -208,7 +179,7 @@ public class ViewEmail extends AppCompatActivity implements ViewMailListener {
     }
 
     private void markWebmailAsRead() {
-        new MailAction(getApplicationContext(), currentUser, "read", "" + currentEmail.contentID, new MailActionListener() {
+        new MailAction(getApplicationContext(), currentUser, getString(R.string.msg_action_read), "" + currentEmail.contentID, new MailActionListener() {
             @Override
             public void onPreMailAction() {
 
@@ -216,7 +187,6 @@ public class ViewEmail extends AppCompatActivity implements ViewMailListener {
 
             @Override
             public void onPostMailAction(boolean success) {
-                Log.d(TAG, "Marking webmail as " + success);
             }
         }).execute();
     }
